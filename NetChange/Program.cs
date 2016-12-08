891e9d474309fa31665dedb02e09afdbda510fad
Robertslob <rjrslob@gmail.com>
2016-12-08 11:22:17 +0100

    opzet

--- NetChange/Program.cs
+++ NetChange/Program.cs
@@ -10,6 +10,18 @@ namespace NetChange
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            string[] ports = s.Split(' ');
+            int ownPort = int.Parse(ports[0]);
+            int[] connectedPorts = new int[ports.Length -1];
+            for(int i = 0; i<ports.Length-1; i++)
+            {
+                connectedPorts[i] = int.Parse(ports[i + 1]);
+                if (ownPort < connectedPorts[i])
+                    ;//make socket 
+            }
+
+            
         }
     }
 }
